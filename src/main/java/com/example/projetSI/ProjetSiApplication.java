e8ba4f6ed6232c1d3cceb5fefd1430ab03c7f617
Meryem EL KARATI <elkaratimeryem@gmail.com>
2023-12-03 11:28:11 +0100

    first commit

---
+++ src/main/java/com/example/projetSI/ProjetSiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetSI/ProjetSiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetSI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
